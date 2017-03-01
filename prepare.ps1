e4be5c20c811b97d13c4eaebf1427448c065e370
Gerard Braad <me@gbraad.nl>
2017-03-01 03:56:51 +0000

    Test

---
+++ prepare.ps1
@@ -0,0 +1,9 @@
+$Url = "https://github.com/minishift/minishift/releases/download/v1.0.0-beta.4/minishift-1.0.0-beta.4-windows-amd64.zip"
+$Downloaded = "$PSScriptRoot\minishift.zip"
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($Url, $Downloaded)
+
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::ExtractToDirectory($Downloaded, $PSScriptRoot)
+
